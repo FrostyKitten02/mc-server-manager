b03584699f9b213b162c55d60d5e7fc2802b3263
FrostyKitten02 <alen.fridau02@gmail.com>
2025-10-15 17:21:30 +0200

    initial

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mc-server-manager
 
 go 1.24
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"mc-server-manager/controller"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	RconPort      uint32 `json:"rconPort"`
+	Ip            string `json:"ip"`
+	ContainerName string `json:"containerName"`
+}
+
+func main() {
+	router := mux.NewRouter()
+	controller.BindServersEndpoints(router)
+	err := http.ListenAndServe(":4000", router)
+	if err != nil {
+		return
+	}
+
+	for {
+
+	}
 }
