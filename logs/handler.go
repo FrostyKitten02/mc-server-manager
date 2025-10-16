cbf41de2c45fab6c2618c226c40a00cdf0327222
FrostyKitten02 <alen.fridau02@gmail.com>
2025-10-16 22:12:21 +0200

    logging

---
+++ logs/handler.go
@@ -0,0 +1,44 @@
+package logs
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+	"time"
+)
+
+type SimpleHandler struct {
+	w    *os.File
+	opts *slog.HandlerOptions
+}
+
+func NewSimpleHandler(w *os.File, opts *slog.HandlerOptions) *SimpleHandler {
+	if opts == nil {
+		opts = &slog.HandlerOptions{}
+	}
+	return &SimpleHandler{w: w, opts: opts}
+}
+
+func (h *SimpleHandler) Enabled(_ context.Context, lvl slog.Level) bool {
+	minLevel := slog.LevelInfo
+	if h.opts.Level != nil {
+		minLevel = h.opts.Level.Level()
+	}
+	return lvl >= minLevel
+}
+
+func (h *SimpleHandler) Handle(_ context.Context, r slog.Record) error {
+	ts := r.Time.Format(time.DateTime)
+
+	levelStr := strings.ToUpper(r.Level.String())
+	levelStr = fmt.Sprintf("%-5s", levelStr)
+
+	line := fmt.Sprintf("[%s] %s %s", ts, levelStr, r.Message)
+	_, err := fmt.Fprintln(h.w, line)
+	return err
+}
+
+func (h *SimpleHandler) WithAttrs(attrs []slog.Attr) slog.Handler { return h }
+func (h *SimpleHandler) WithGroup(name string) slog.Handler       { return h }
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"mc-server-manager/conf"
 	"mc-server-manager/httpserver"
+	"mc-server-manager/logs"
 	"mc-server-manager/ws"
 )
 
 func main() {
+	logs.InitLogger()
 	conf.Load()
 
 	go httpserver.StartHttpServer(conf.Conf.ServerPort)
